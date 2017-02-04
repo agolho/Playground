a2d17b506cb238e1979af7f7a780ccd7b7a1614c
Yusuf <agolho@gmail.com>
2017-02-05 00:16:27 +0300

    Forgot the animation

--- json/index.js
+++ json/index.js
@@ -1,6 +1,7 @@
 var str;
 $("#buton").on("click", function(){
   genQuote();
+  $(".quote").addClass("animated bounce");
 });
 /*
 $("#tweet").on("click", function(){
@@ -27,6 +28,11 @@ function prepareTweet(foo){
   var appendLink = 'https://twitter.com/intent/tweet?hashtags=quotes&related=ysfbekts&text='+encodeURIComponent(foo)+" @ysfbekts";
   var buttonCon='<a href="' + appendLink +'" target="_blank" class="btn btn-lg btn-info btn-block"><i class="fa fa-twitter"></i> Tweet!</a>';
   $(".conTweet").html(buttonCon);
+  setTimeout(func, 1000);
+  function func() {
+      $(".quote").removeClass("animated bounce");
+  }
+
 }
 
 /* HOW THEY DIT IT
