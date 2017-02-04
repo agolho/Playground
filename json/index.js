15a0060854a9d47efaefb19d36763268a511f0dc
Yusuf <agolho@gmail.com>
2017-02-05 00:30:50 +0300

    Redundancy code removed.

--- json/index.js
+++ json/index.js
@@ -3,11 +3,6 @@ $("#buton").on("click", function(){
   genQuote();
   $(".quote").addClass("animated bounce");
 });
-/*
-$("#tweet").on("click", function(){
-  tweet();
-});
-*/
 function genQuote () {
     $.ajax({
       url: "https://api.icndb.com/jokes/random",
@@ -34,53 +29,3 @@ function prepareTweet(foo){
   }
 
 }
-
-/* HOW THEY DIT IT
-function getQuote() {
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(response) {
-      var r = JSON.parse(response);
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-      if(inIframe())
-      {
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-      }
-      $(".quote-text").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#text').text(r.quote);
-        });
-
-      $(".quote-author").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#author').html(r.author);
-        });
-
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color],
-        color: colors[color]
-      }, 1000);
-      $(".button").animate({
-        backgroundColor: colors[color]
-      }, 1000);
-    }
-  });
-}
-*/
