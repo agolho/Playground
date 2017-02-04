ea2d83fdcc667e8ae0b17176cae80e65ffa8ef27
Yusuf <agolho@gmail.com>
2017-02-04 14:01:54 +0300

    https

--- json/index.js
+++ json/index.js
@@ -1,5 +1,5 @@
 $("#buton").on("click", function(){
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
     $(".data").append(a[0].content + "<p>— " + a[0].title + "</p>")
   });
 });
