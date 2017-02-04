92001167ba150f4d9577b5a0ead05aee89b46581
Yusuf <agolho@gmail.com>
2017-02-04 14:03:07 +0300

    changed json

--- json/index.js
+++ json/index.js
@@ -1,5 +1,5 @@
 $("#buton").on("click", function(){
-  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $.getJSON("https://gist.githubusercontent.com/signed0/d70780518341e1396e11/raw/2a7f4af8d181a714f9d49105ed57fafb3f450960/quotes.json", function(a) {
     $(".data").append(a[0].content + "<p>— " + a[0].title + "</p>")
   });
 });
