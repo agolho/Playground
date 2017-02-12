00b6b140e5cecb9fb3e9c9a03b4a17f584f54eda
Yusuf <agolho@gmail.com>
2017-02-12 13:55:08 +0300

    Computer player Boilerplate

--- ttt/index.js
+++ ttt/index.js
@@ -1,4 +1,9 @@
-var osturn,isGameOver,boxes=[],turn=0;
+var osturn,compPlayer,compTurn,startWith,isGameOver,boxes=[],turn=0;
+//Starting Routine
+$('.box').addClass('pff');
+$('.promtBox').addClass('pff invisible');
+$('#gameBoard').addClass('invisible');
+$('#header').addClass('animated pulse');
 
 if (!isGameOver){
   $('#a1').click(function(){boxInput('a1')});
@@ -11,20 +16,52 @@ if (!isGameOver){
   $('#c2').click(function(){boxInput('c2')});
   $('#c3').click(function(){boxInput('c3')});
 }
+$("#p2p").click(function(){
+  $('.promtBox').removeClass('pff invisible');
+  $('#header').addClass('animated pulse');
+  $('.promt2Box').addClass('pff invisible');
+});
+$("#ai").click(function(){
+  $('.promtBox').removeClass('pff invisible');
+  $('#header').addClass('animated pulse');
+  $('.promt2Box').addClass('pff invisible');
+  compPlayer=true;
+});
+$("#you").click(function(){
+  startWith=true;
+  if(compPlayer) compTurn=true;
+  commenceGame();
+});
+$("#me").click(function(){
+  startWith=false;
+  commenceGame();
+});
 
+function commenceGame(){
+  if(!startWith){
+    osturn=false;
+  } else {
+    osturn=true;
+  }
+  isGameOver=false;
+  deepThink();
+  $('.box').removeClass('pff')
+  $('#gameBoard').removeClass('invisible');
+  $('#header').removeClass('invisible');
+  $('#promt').addClass('invisible pff');
+}
 function gameOver(){
   isGameOver=true;
+  $('.box').addClass('pff');
   $('#gameBoard').addClass('invisible');
-  $('#header').addClass('animated pulse down');
+  $('#header').addClass('animated pulse');
   setTimeout(function() {gameReset();}, 1000);
 }
 function gameReset(){
   boxes=[];
-  isGameOver=false;
-  osturn=false;
   turn=0;
-  $('#gameBoard').removeClass('invisible');
-  $('#header').removeClass('animated pulse down');
+  commenceGame();
+  $('#header').removeClass('animated pulse');
   $('#header').html('Tic - Tac - Toe');
   for(i=0;i<4;i++){
     $('#a'+i).html('');
@@ -33,7 +70,7 @@ function gameReset(){
   }
 }
 function boxInput(box){
-  if ($('#'+box).text() !== 'O' && $('#'+box).text() !== 'X'){
+  if ($('#'+box).text() !== 'O' && $('#'+box).text() !== 'X' && !compTurn){
     if (osturn){
       $('#'+box).html('O');
       didWin(box);
@@ -44,12 +81,26 @@ function boxInput(box){
       osturn = !osturn;
     }
   }
+  deepThink();
 }
-
+function deepThink(){
+  if (compTurn){
+    var seek=[[$('#a1').text(),'a1'],[$('#a2').text(),'a2'],[$('#a3').text(),'a3'],[$('#b1').text(),'b1'],[$('#b2').text(),'b2'],[$('#b3').text(),'b3'],[$('#c1').text(),'c1'],[$('#c2').text(),'c2'],[$('#c3').text(),'c3']];
+    var filterSeek = seek[0].filter(isEmpty);
+    console.log(filterSeek);
+  }
+}
+function isEmpty(value) {
+  return value == '';
+}
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function didWin(box){
   turn++;
   boxes=[$('#a1').text(),$('#a2').text(),$('#a3').text(),$('#b1').text(),$('#b2').text(),$('#b3').text(),$('#c1').text(),$('#c2').text(),$('#c3').text()];
-  console.log(boxes);
   if(boxes[0] == boxes[1] && boxes[1] == boxes[2] && boxes[0]!=''){
     $('#header').html(boxes[0] +' Won!');
     gameOver();
