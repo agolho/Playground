9c094246df5fbb0ec9e6be577fcb6d2b8c12071d
Yusuf <agolho@gmail.com>
2017-02-13 05:59:32 +0300

    Finished Random AI

--- ttt/index.js
+++ ttt/index.js
@@ -34,26 +34,30 @@ $("#you").click(function(){
 });
 $("#me").click(function(){
   startWith=false;
+  if(compPlayer) compTurn=false;
   commenceGame();
 });
 
 function commenceGame(){
   if(!startWith){
     osturn=false;
+    if(compPlayer) compTurn=false;
   } else {
     osturn=true;
+    if(compPlayer) compTurn=true;
+    deepThink();
   }
   isGameOver=false;
-  deepThink();
-  $('.box').removeClass('pff')
+  $('.box').removeClass('pff');
+  $('#gameBoard').removeClass('animated fadeOut');
   $('#gameBoard').removeClass('invisible');
   $('#header').removeClass('invisible');
   $('#promt').addClass('invisible pff');
 }
 function gameOver(){
   isGameOver=true;
-  $('.box').addClass('pff');
-  $('#gameBoard').addClass('invisible');
+  $('#gameBoard').addClass('animated fadeOut');
+  setTimeout(function() {$('#gameBoard').addClass('invisible');}, 500);
   $('#header').addClass('animated pulse');
   setTimeout(function() {gameReset();}, 1000);
 }
@@ -74,20 +78,37 @@ function boxInput(box){
     if (osturn){
       $('#'+box).html('O');
       didWin(box);
-      osturn = !osturn;
+      if (!startWith){osturn = !osturn;}
+      if(compPlayer) compTurn=true;
+      if(!isGameOver) deepThink();
     } else if (!osturn){
       $('#'+box).html('X');
       didWin(box);
       osturn = !osturn;
+      if(compPlayer) compTurn=true;
+      if(!isGameOver) deepThink();
     }
   }
-  deepThink();
 }
+var options=['a1','a2','a3','b1','b2','b3','c1','c2','c3'];
+var pawn = 'X';
 function deepThink(){
+  if(compPlayer){$('#thinkInd').html('<i class="fa fa-cog fa-spin fa-fw"></i>');}
   if (compTurn){
-    var seek=[[$('#a1').text(),'a1'],[$('#a2').text(),'a2'],[$('#a3').text(),'a3'],[$('#b1').text(),'b1'],[$('#b2').text(),'b2'],[$('#b3').text(),'b3'],[$('#c1').text(),'c1'],[$('#c2').text(),'c2'],[$('#c3').text(),'c3']];
-    var filterSeek = seek[0].filter(isEmpty);
-    console.log(filterSeek);
+    var shot = getRandomIntInclusive(0,8);
+    console.log(options[shot]);
+    if (!startWith) pawn='O';
+    if($('#'+options[shot]).text()==''){
+      setTimeout(function() {
+        if(!isGameOver){
+          $('#'+options[shot]).html(pawn);
+          $('#thinkInd').html('&nbsp');
+          didWin(options[shot]);
+          compTurn=false;
+        }
+      }, 400);
+      if(pawn=='O') osturn = !osturn;
+    } else if(turn<9 && !isGameOver){setTimeout(function() {deepThink();}, 400);} else didWin(options[shot]);
   }
 }
 function isEmpty(value) {
@@ -101,41 +122,41 @@ function getRandomIntInclusive(min, max) {
 function didWin(box){
   turn++;
   boxes=[$('#a1').text(),$('#a2').text(),$('#a3').text(),$('#b1').text(),$('#b2').text(),$('#b3').text(),$('#c1').text(),$('#c2').text(),$('#c3').text()];
+  console.log(boxes);
   if(boxes[0] == boxes[1] && boxes[1] == boxes[2] && boxes[0]!=''){
-    $('#header').html(boxes[0] +' Won!');
+    $('#header').html(boxes[0] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[3] == boxes[4] && boxes[4] == boxes[5] && boxes[3]!=''){
-    $('#header').html(boxes[3] +' Won!');
+    $('#header').html(boxes[3] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[6] == boxes[7] && boxes[7] == boxes[8] && boxes[6]!=''){
-    $('#header').html(boxes[6] +' Won!');
+    $('#header').html(boxes[6] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[0] == boxes[3] && boxes[3] == boxes[6] && boxes[0]!=''){
-    $('#header').html(boxes[0] +' Won!');
+    $('#header').html(boxes[0] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[1] == boxes[4] && boxes[4] == boxes[7] && boxes[1]!=''){
-    $('#header').html(boxes[1] +' Won!');
+    $('#header').html(boxes[1] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[2] == boxes[5] && boxes[5] == boxes[8] && boxes[2]!=''){
-    $('#header').html(boxes[2] +' Won!');
+    $('#header').html(boxes[2] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[0] == boxes[4] && boxes[4] == boxes[8] && boxes[0]!=''){
-    $('#header').html(boxes[0] +' Won!');
+    $('#header').html(boxes[0] +' Won!');isGameOver=true;
     gameOver();
   }
   if(boxes[2] == boxes[4] && boxes[4] == boxes[6] && boxes[2]!=''){
-    $('#header').html(boxes[2] +' Won!');
+    $('#header').html(boxes[2] +' Won!');isGameOver=true;
     gameOver();
   }
-
   if( turn==9 && !isGameOver){
-    $('#header').html('Draw.');
+    $('#header').html('Draw.');isGameOver=true;
     gameOver();
   }
 }
