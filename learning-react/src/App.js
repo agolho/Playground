645f71e38932e33ec3ae9205f9a6d323a9ce85ea
Yusuf <agolho@gmail.com>
2017-02-15 14:50:43 +0300

    React Trial-Errors

---
+++ learning-react/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Remarkable from 'remarkable';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+     super(props);
+     this.handleChange = this.handleChange.bind(this);
+     this.state = {value: 'Type some *markdown* here!'};
+   }
+
+   handleChange() {
+     this.setState({value: this.refs.textarea.value});
+   }
+
+   getRawMarkup() {
+     var md = new Remarkable();
+     return { __html: md.render(this.state.value) };
+   }
+
+  render() {
+    return (
+      <div className="App">
+      <h3>Input</h3>
+      <textarea
+        onChange={this.handleChange}
+        ref="textarea"
+        defaultValue={this.state.value} />
+      <h3>Output</h3>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={this.getRawMarkup()}
+      />
+      </div>
+    );
+  }
+}
+
+export default App;
